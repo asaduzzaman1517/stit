7b0019042c01d688fbc8c2db67519bd093845abd
asaduzzaman1517 <asaduzzaman1517@gmail.com>
2020-01-14 11:52:54 +0600

    Header with submenu + hero

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,52 @@
 (function($) {
-    "use strict";
+	"use strict";
 
-    $(document).ready(function() {
-        // Toogle class on navbar
-        $(".kdv_brand__icon-box").on("click", function() {
-            $(this).toggleClass("active");
-            $(".kdv_menu").toggleClass("active");
-        });
-        $(".kdv_menu__close").on("click", function() {
-            $(".kdv_menu").toggleClass("active");
-            $(".kdv_brand__icon-box").toggleClass("active");
-        });
+	$(document).ready(function() {
+		/**-----------------------------
+		 *  Navbar fix
+		 * ---------------------------*/
 
-        //Hero Slider
-        var heroSlider = $(".kdv_hero__slider");
-        if (heroSlider.length) {
-            heroSlider.slick({
-                slidesToShow: 1,
-                mobileFirst: true,
-                fade: true,
-                cssEase: 'linear',
-                arrows: false,
-                autoplay: true,
-                autoplaySpeed: 8000,
-                speed: 800,
-                responsive: [
-                    {
-                        breakpoint: 767,
-                        settings: {
-                            slidesToShow: 3,
-                            fade: false,
-                            centerMode: true,
-                            centerPadding: '0'
-                        }
-                    }
-                ]
-            });
-        }
-    });
+		$(document).on(
+			"click",
+			"li.menu-item-has-children>a",
+			function(e) {
+				e.preventDefault();
+			}
+		);
+		// Toogle class on navbar
+		$(".sdy_brand__icon-box").on("click", function() {
+			$(this).toggleClass("active");
+			$(".sdy_menu").toggleClass("active");
+		});
+		$(".sdy_menu__close").on("click", function() {
+			$(".sdy_menu").toggleClass("active");
+			$(".sdy_brand__icon-box").toggleClass("active");
+		});
+
+		//Hero Slider
+		var heroSlider = $(".sdy_hero__slider");
+		if (heroSlider.length) {
+			heroSlider.slick({
+				slidesToShow: 1,
+				mobileFirst: true,
+				fade: true,
+				cssEase: "linear",
+				arrows: false,
+				autoplay: true,
+				autoplaySpeed: 8000,
+				speed: 800,
+				responsive: [
+					{
+						breakpoint: 767,
+						settings: {
+							slidesToShow: 3,
+							fade: false,
+							centerMode: true,
+							centerPadding: "0"
+						}
+					}
+				]
+			});
+		}
+	});
 })(jQuery);
