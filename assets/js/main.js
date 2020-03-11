4349f99c77f2160e101ceac14b83342d65e37680
asaduzzaman1517 <lallsobuz@gmail.com>
2020-03-11 19:18:20 +0600

    hero done

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,5 +36,64 @@
 				nextArrow: '<i class="fas fa-angle-right slick-arrow-next"></i>'
 			});
 		}
+		//Hero Image Animation
+		var hoverLayer = $('.st-hero-bg');
+
+		var heroImgOne = $('.st-hero-bg__img-two');
+		var heroImgTwo = $('.st-hero-bg__img-three');
+		var heroImgThree = $('.st-hero-bg__img-four');
+		var heroImgFour = $('.st-hero-bg__img-five');
+		var heroImgFive = $('.st-hero-bg__img-six');
+		var heroImgSix = $('.st-hero-bg__img-seven');
+
+		//Animation Init
+		hoverLayer.mousemove(function(e) {
+			var valueX = (e.pageX * -1 / 112);
+			var valueY = (e.pageY * -1 / 112);
+
+			heroImgOne.css({
+				'transform': 'translate3d('+valueX+'px,'+valueY+'px, 0)'
+			})
+		});
+		hoverLayer.mousemove(function(e) {
+			var valueX = (e.pageX * -1 / 115);
+			var valueY = (e.pageY * -1 / 150);
+
+			heroImgTwo.css({
+				'transform': 'translate3d('+valueX+'px,'+valueY+'px, 0)'
+			})
+		});
+		hoverLayer.mousemove(function(e) {
+			var valueX = (e.pageX * -1 / 20);
+			var valueY = (e.pageY * -1 / 25);
+
+			heroImgThree.css({
+				'transform': 'translate3d('+valueX+'px,'+valueY+'px, 0)'
+			})
+		});
+		hoverLayer.mousemove(function(e) {
+			var valueX = (e.pageX * -1 / 30);
+			var valueY = (e.pageY * -1 / 35);
+
+			heroImgFour.css({
+				'transform': 'translate3d('+valueX+'px,'+valueY+'px, 0)'
+			})
+		});
+		hoverLayer.mousemove(function(e) {
+			var valueX = (e.pageX * -1 / 60);
+			var valueY = (e.pageY * -1 / 65);
+
+			heroImgFive.css({
+				'transform': 'translate3d('+valueX+'px,'+valueY+'px, 0)'
+			})
+		});
+		hoverLayer.mousemove(function(e) {
+			var valueX = (e.pageX * -1 / 60);
+			var valueY = (e.pageY * -1 / 65);
+
+			heroImgSix.css({
+				'transform': 'translate3d('+valueX+'px,'+valueY+'px, 0)'
+			})
+		});
 	});
 })(jQuery);
